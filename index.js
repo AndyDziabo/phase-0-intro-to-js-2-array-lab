a4e8bbeead2d89e5081524db4a9a1310a0e8392e
Andy Dziabo <jameszabo@yahoo.com>
2022-08-09 09:46:37 -0400

    commit tree 9c5cfb2c7711275cea698f0da026048af839b2c7

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const copyOfCats = cats.slice();
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+function prependCat(name){
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
